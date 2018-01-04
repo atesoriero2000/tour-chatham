8f451344954ad358953941b963a4e608194a148f
anthonyTesoriero <anthony.tesoriero>
2018-01-03 23:00:02 -0500

    1.1 Update
    + Added Martha wells to the contributors page
    + Changed App name on iTunes connect

--- app/components/ios/about.js
+++ app/components/ios/about.js
@@ -55,7 +55,7 @@ class About extends Component {
           <View>
             <Text allowFontScaling = {false} style = {styles.labels}>Historical Society</Text>
             <Text allowFontScaling = {false} style = {styles.fineText}>
-              {'Pat Wells\nSheila Goggins\nDebbie Bucuk\nCaroline Knott'}
+              {'Pat Wells\nMartha Wells\nSheila Goggins\nDebbie Bucuk\nCaroline Knott'}
             </Text>
           </View>
 

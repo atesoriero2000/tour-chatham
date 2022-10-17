cd2f9d521d4416b777c7ea2c3de4e11ef6c1984b
Anthony Tesoriero <atesoriero2000@gmail.com>
2022-10-16 23:46:01 -0400

    react-native-background-geolocation upgraded to 4.8.2
    builds and deployes but geolocation functionality not yet tested
    Welcome page and About page are working
    react-native-background-fetch upgraded to 4.1.3
    hermes enabled
    App icon and splashscreen transfered
    Build declared to v2.0

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+// import React, { Component } from 'react';
+import {
+    AppRegistry,
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
